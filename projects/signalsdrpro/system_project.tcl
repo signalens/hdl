270d157e495dc0eef8a30ff51e536aaab4f68cb5
xwings <kj@xandora.net>
2024-08-23 17:20:43 +0800

    add sdrpro

--- projects/signalsdrpro/system_project.tcl
+++ projects/signalsdrpro/system_project.tcl
@@ -4,14 +4,14 @@ source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 set p_device "xc7z020clg400-1"
-adi_project signalsdrpi
+adi_project signalsdrpro
 
-adi_project_files signalsdrpi [list \
+adi_project_files signalsdrpro [list \
   "system_top.v" \
   "system_constr.xdc" \
   "$ad_hdl_dir/library/common/ad_iobuf.v"]
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
-adi_project_run signalsdrpi
+adi_project_run signalsdrpro
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
 
