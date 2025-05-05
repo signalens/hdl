3f405e6edeb23f165d0a979df595afe1873ad244
xwings <code@emulate.sh>
2025-05-05 10:35:16 +0800

    Update export_hardware.tcl

--- projects/signalsdrpro/export_hardware.tcl
+++ projects/signalsdrpro/export_hardware.tcl
@@ -1,7 +1,4 @@
-puts "Current directory: [pwd]"
+puts "Run export_hardware.tcl"
 open_project signalsdrpro.xpr
-launch_runs impl_1 -to_step write_bitstream -jobs 4
-wait_on_run impl_1
-open_run impl_1
 write_hw_platform -fixed -include_bit -force -file signalsdrpro.sdk/system_top.xsa
 close_project 
