8c69131a484b536dc0c20a8221d2ea80c801d83d
xwings <code@emulate.sh>
2025-05-05 10:00:42 +0800

    Update system_project.tcl

--- projects/signalsdrpro/system_project.tcl
+++ projects/signalsdrpro/system_project.tcl
@@ -1,7 +1,6 @@
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
-source ./system_bd.tcl
 
 set p_device "xc7z020clg400-1"
 adi_project signalsdrpro
