cf40a599e9f80269bb9f82d78779e07613ec125a
xwings <code@emulate.sh>
2025-05-06 20:24:50 +0800

    Update system_project.tcl

--- projects/signalsdrpro/system_project.tcl
+++ projects/signalsdrpro/system_project.tcl
@@ -12,5 +12,6 @@ adi_project_files signalsdrpro [list \
 
 set_property STEPS.WRITE_BITSTREAM.TCL.PRE [list [file join $ad_hdl_dir projects scripts write_bitstream_pre.tcl]] [get_runs impl_1]
 
-adi_project_run signalsdrpro
 source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
+adi_project_run signalsdrpro
+
